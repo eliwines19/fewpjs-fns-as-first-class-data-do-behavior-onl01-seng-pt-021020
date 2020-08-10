0e1304493b9c89efb6eca7ee3d0469882eadcce8
Elijah wines <elijahwines84@yahoo.com>
2020-08-10 19:35:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function handleClick(e) {
 function greet(timeString) {
   const hour = parseInt(timeString, 10);
   if (hour < 12) return "Good Morning";
-  if (hour > 17) return "Good Eveningt";
+  if (hour > 17) return "Good Evening";
   return "Good Afternoon";
 }
 /* Write your implementation of displayMessage() */
